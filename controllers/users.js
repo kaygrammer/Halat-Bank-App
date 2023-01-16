dc144f5155c268fcc48d4c6b03bd351443e9e270
Olaoye Kayode Samuel <kayodeolaoye18@gmail.com>
2023-01-16 17:29:28 +0100

    general project setup and defining of the database model

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ const createUser = async (req, res) => {
     
     try{
         const user = await Users.create(req.body)
-        res.status(200).json("user created successfull", user)
+        res.status(200).json("user created successfully", user)
     }catch(err){
         res.status(500).json(err);
     }
